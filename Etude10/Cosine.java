15e917afb93058daa70be76fb269d25a44ad88ff
daanikus <daanikus@gmail.com>
2018-05-20 16:01:46 +1200

    Changed some stuff

--- Etude10/Cosine.java
+++ Etude10/Cosine.java
@@ -6,22 +6,24 @@ public class Cosine {
 
 
     public static void main(String[] args) {
-        System.out.println(solve());
-        System.out.println(Math.cos(0.998));
+        Double x = 12.345;
+        System.out.println("Our result: " + solve(x));
+        System.out.println("Our result better: " + betterSolve(x));
+        System.out.println("Math lib result: " + Math.cos(x));
     }
     
-    public static Double solve() {
+    public static Double solve(Double x) {
         //1 - x/2! + x/4! - x/8! ...
         Double denom = 2d;
         Boolean minus = true;
         Double ans = 1.0;
-        Double x = 0.998;
         for (int i = 0; i < 5000; i++) {
             if (minus) ans -= Math.pow(x, denom) / factorial(denom);
-            else ans += Math.pow(x, denom) / factorial(denom);
-            System.out.println(denom);
+            else ans *= -Math.pow(x, denom) / factorial(denom);
+            System.out.println(ans);
             denom += 2;
             minus = !minus;
+            
         }
         return ans;
     }
@@ -33,5 +35,14 @@ public class Cosine {
         }
         return ans;
     }
-    
+
+    public static Double betterSolve(Double x) {
+        Double ans = 1d;
+        Double prev = 1d;
+        for (int i = 5000; i >= 1; i++) {
+            ans = -(Math.pow(x, 2) / (2*i)*(2*i - 1) * ans);
+            //prev = ans;
+        }
+        return ans;
+    }
 }
