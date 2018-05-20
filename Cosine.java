2c7960c0793f19c7124c693f7780594b526d65a6
daanikus <daanikus@gmail.com>
2018-05-20 14:07:18 +1200

    Changed something

--- Cosine.java
+++ Cosine.java
@@ -7,17 +7,19 @@ public class Cosine {
 
     public static void main(String[] args) {
         System.out.println(solve());
+        System.out.println(Math.cos(0.998));
     }
     
     public static Double solve() {
         //1 - x/2! + x/4! - x/8! ...
-        Double denom = 1d;
+        Double denom = 2d;
         Boolean minus = true;
         Double ans = 1.0;
         Double x = 0.998;
         for (int i = 0; i < 5000; i++) {
             if (minus) ans -= Math.pow(x, denom) / factorial(denom);
             else ans += Math.pow(x, denom) / factorial(denom);
+            System.out.println(denom);
             denom += 2;
             minus = !minus;
         }
